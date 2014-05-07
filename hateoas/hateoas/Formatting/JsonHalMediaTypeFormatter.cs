7b3708a271a4022f7bff3ce84d9072b666ba884c
Mike Haas <mike@emken.co>
2014-05-07 13:03:50 -0500

    wip

---
+++ hateoas/hateoas/Formatting/JsonHalMediaTypeFormatter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+
+namespace Hateoas.Formatting
+{
+    public class JsonHalMediaTypeFormatter : BufferedMediaTypeFormatter
+    {
+        public JsonHalMediaTypeFormatter()
+        {
+            SupportedMediaTypes.Add(new MediaTypeHeaderValue("application/json+hal"));
+        }
+
+        public override bool CanReadType(Type type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool CanWriteType(Type type)
+        {
+            return false;
+        }
+    }
+}
