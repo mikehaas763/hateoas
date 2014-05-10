a4080404d70f14e7baa20aa52b4181206199c078
Mike Haas <mike@emken.co>
2014-05-09 21:30:01 -0500

    Preparation for a much more scalable library.

---
+++ hateoas/hateoas/HalStructure.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hateoas
+{
+    class HalStructure
+    {
+        public IList<Link> Links { get; set; }
+
+    }
+}
---
+++ hateoas/hateoas/Link.cs
@@ -0,0 +1,8 @@
+﻿namespace Hateoas
+{
+    internal class Link
+    {
+        public string href { get; set; }
+        public string name { get; set; }
+    }
+}
